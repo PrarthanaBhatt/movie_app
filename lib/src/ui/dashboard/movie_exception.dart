5597f5ac091c68da5c2a14f956bfabb44e6c70a8
PrarthanaB <PrarthanaB@winjit.com>
2023-07-13 18:44:42 +0530

    movie api call

---
+++ lib/src/ui/dashboard/movie_exception.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+
+class MoviesException implements Exception {
+  MoviesException.fromDioError(DioError dioError) {
+    switch (dioError.type) {
+      case DioErrorType.cancel:
+        message = "Request to API server was cancelled";
+        break;
+      case DioErrorType.receiveTimeout:
+        message = "Receive timeout in connection with API server";
+        break;
+      case DioErrorType.sendTimeout:
+        message = "Send timeout in connection with API server";
+        break;
+      default:
+        message = "Something went wrong";
+        break;
+    }
+  }
+
+  String? message;
+
+  @override
+  String toString() => message.toString();
+}
---
+++ lib/src/ui/dashboard/movie_service.dart
@@ -0,0 +1,38 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movie_app/src/ui/dashboard/movie.dart';
+import 'package:movie_app/src/ui/dashboard/movie_exception.dart';
+import 'package:movie_app/src/utils/enviroment_config.dart';
+
+final movieServiceProvider = Provider<MovieService>((ref) {
+  final config = ref.read(enviromentConfigProvider);
+
+  return MovieService(config, Dio());
+});
+
+class MovieService {
+  final EnviromentConfig _enviromentConfig;
+  final Dio _dio;
+
+  MovieService(this._enviromentConfig, this._dio);
+
+  Future<List<Movie>> getMovies() async {
+    try {
+      final response = await _dio.get(
+        // ${_enviromentConfig.movieApiKey}
+
+        "https://api.themoviedb.org/3/movie/popular?api_key=e1eb9d73afc601612fb492d7f0e060aa&language=en-US&page=1",
+      );
+
+      final results = List<Map<String, dynamic>>.from(response.data['results']);
+
+      List<Movie> movies = results
+          .map((movieData) => Movie.fromMap(movieData))
+          .toList(growable: false);
+
+      return movies;
+    } on DioError catch (dioError) {
+      throw MoviesException.fromDioError(dioError);
+    }
+  }
+}
