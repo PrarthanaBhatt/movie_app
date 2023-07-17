442fa9f8fc6e4cb716addcbf831b9741d9fe7e77
PrarthanaB <PrarthanaB@winjit.com>
2023-07-17 14:35:04 +0530

    add  sliver appbar and listtile widget

--- lib/src/components/widgets/movie_list_tile.dart
+++ lib/src/components/widgets/movie_list_tile.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:movie_app/src/components/widgets/movie_title_widget.dart';
 import 'package:movie_app/src/models/movie.dart';
 
 class MovieListTile extends StatelessWidget {
@@ -12,21 +11,63 @@ class MovieListTile extends StatelessWidget {
     return Card(
       elevation: 0,
       color: Theme.of(context).colorScheme.surfaceVariant,
-      child: Stack(
+      child: Row(
         children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.network(
-              movie.fullImageUrl,
-              fit: BoxFit.cover,
-              width: double.maxFinite,
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.network(
+                movie.fullImageUrl,
+                fit: BoxFit.cover,
+                width: 80,
+              ),
             ),
           ),
-          Positioned(
-            bottom: 0,
-            left: 0,
-            right: 0,
-            child: MovieTitleWidget(text: movie.title),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.10,
+            width: MediaQuery.of(context).size.width * 0.55,
+            child: Column(
+              children: [
+                Flexible(
+                  child: Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                      movie.title,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(6.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: const [
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.black45,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Icon(
+                          Icons.add_circle_outline,
+                          color: Colors.black45,
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Icon(
+                          Icons.remove_circle_outline,
+                          color: Colors.black45,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
--- lib/src/ui/movie_detail/movie_details.dart
+++ lib/src/ui/movie_detail/movie_details.dart
@@ -6,7 +6,6 @@ class MovieDetails extends ConsumerStatefulWidget {
   final String backdropPath;
   final String releaseDate;
   final String originalLanguage;
-  // final String voteAverage;
   final String originalTitle;
 
   const MovieDetails({
@@ -15,7 +14,6 @@ class MovieDetails extends ConsumerStatefulWidget {
     required this.backdropPath,
     required this.releaseDate,
     required this.originalLanguage,
-    // required this.voteAverage,
     required this.originalTitle,
   });
 
@@ -27,58 +25,84 @@ class _MovieDetailsState extends ConsumerState<MovieDetails> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("Movie Details"),
-          backgroundColor: Colors.black45,
-        ),
-        body: Column(
-          children: [
-            Image.network(
+        body: CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          centerTitle: true,
+          backgroundColor: Colors.black87,
+          pinned: true,
+          flexibleSpace: FlexibleSpaceBar(
+            centerTitle: true,
+            background: Image.network(
               widget.backdropPath,
               fit: BoxFit.fill,
               width: double.maxFinite,
             ),
-            Padding(
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      widget.originalTitle,
-                      style: const TextStyle(fontSize: 20),
+          ),
+          expandedHeight: 230,
+        ),
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        widget.originalTitle,
+                        style: const TextStyle(fontSize: 20),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Language: ${widget.originalLanguage}"),
-                        Text("Release On:${widget.releaseDate}"),
-                      ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text("Language: ${widget.originalLanguage}"),
+                          Text("Release On:${widget.releaseDate}"),
+                        ],
+                      ),
                     ),
-                  ),
-                  const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                          "Overview:",
-                          style: TextStyle(fontSize: 14),
-                        )),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      widget.overview,
-                      style: const TextStyle(fontSize: 12),
+                    const Padding(
+                      padding: EdgeInsets.all(8.0),
+                      child: Align(
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "Overview:",
+                            style: TextStyle(fontSize: 14),
+                          )),
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        widget.overview,
+                        style: const TextStyle(fontSize: 12),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => ListTile(
+              tileColor: (index % 2 == 0) ? Colors.white : Colors.green[50],
+              title: Center(
+                child: Text('$index',
+                    style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        fontSize: 50,
+                        color: Colors.black45)),
               ),
             ),
-          ],
-        ));
+            childCount: 21,
+          ),
+        )
+      ],
+    ));
   }
 }
